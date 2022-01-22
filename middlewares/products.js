5f5b25981e0bae1a312a3eede381f35020f1807a
Quy <hnquy.hbt@gmail.com>
2022-01-22 16:09:08 +0700

    FIX show acp when login with admin authorization

--- middlewares/products.js
+++ middlewares/products.js
@@ -1,4 +1,4 @@
-const ProductsModel = require(__path_models + 'products');
+const GroupsModel = require(__path_models + 'groups');
 const CategoriesModel = require(__path_models + 'categories');
 const SettingsModel = require(__path_models + 'settings');
 const UsersModel = require(__path_models + 'users');
@@ -7,6 +7,9 @@ const SystemConfigs = require(__path_configs + 'system');
 
 module.exports = async (req, res, next) => {
 	const user = req.user ? req.user : await UsersModel.getItem(SystemConfigs.guest_id);
+	res.locals.isAdmin = false;
+	const groupOfUser = user.group && user.group.id ? await GroupsModel.getItem(user.group.id) : false;
+    if (groupOfUser && groupOfUser.group_acp === 'yes') res.locals.isAdmin = true;
 	res.locals.user = user;
 	const cartProducts = await CartModel.getCartProducts(user.cart, null);
 	res.locals.cartProducts = cartProducts.products;
