28041272c047ad32459c26829b00704885a0fb75
Quy <hnquy.hbt@gmail.com>
2022-02-16 00:28:30 +0700

    ADD controller orders frontend

--- controllers/frontend/orders.js
+++ controllers/frontend/orders.js
@@ -1,5 +1,3 @@
-const express = require('express');
-const router = express.Router();
 const util = require('util');
 
 const OrderModel = require(__path_models + 'orders');
@@ -10,35 +8,28 @@ const collectionName = 'orders';
 const folderView = `${__path_views_frontend}pages/${collectionName}`;
 const layout = __path_views_frontend + 'layouts/layout';
 const pageTitle = 'Đơn hàng của bạn';
-const linkIndex = '/' + collectionName;
 
-/* GET ORDER PAGE */
-router.get('/', async (req, res, next) => {
+module.exports = {
+	index: async (req, res, next) => {
+		res.render(`${folderView}/index`, { 
+			layout,
+			pageTitle, 
+		});
+	},
+	getOrder: async (req, res, next) => {
+		const { code } = req.params;
 	
-	res.render(`${folderView}/index`, { 
-		layout,
-		pageTitle, 
-	});
-});
-
-/* GET ORDER DATA */
-router.get('/:code', async (req, res, next) => {
-	const { code } = req.params;
-
-	const data = await OrderModel.getItemFrontend(code);
-	res.send({data});
-});
-
-/* POST NEW ORDER */
-router.post('/add', async (req, res, next) => {
-	const item = JSON.parse(req.body.data);
-	const user = req.user;
-
-	const data = await OrderModel.saveItem(item, {task: 'add', user});
+		const data = await OrderModel.getItemFrontend(code);
+		res.send({data});
+	},
+	addOrder: async (req, res, next) => {
+		const item = JSON.parse(req.body.data);
+		const user = req.user;
 	
-	await mailerHelpers.sendMail(user.email, notifyConfigs.MAIL_TITLE_ORDER, util.format(notifyConfigs.MAIL_CONTENT_ORDER, data.code));
-
-	res.send(data);
-});
-
-module.exports = router;
+		const data = await OrderModel.saveItem(item, {task: 'add', user});
+		
+		await mailerHelpers.sendMail(user.email, notifyConfigs.MAIL_TITLE_ORDER, util.format(notifyConfigs.MAIL_CONTENT_ORDER, data.code));
+	
+		res.send(data);
+	}
+}
--- routes/frontend/orders.js
+++ routes/frontend/orders.js
@@ -1,44 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const util = require('util');
-
-const OrderModel = require(__path_models + 'orders');
-const mailerHelpers = require(__path_helpers + 'mailer');
-const notifyConfigs = require(__path_configs + 'notify');
 
 const collectionName = 'orders';
-const folderView = `${__path_views_frontend}pages/${collectionName}`;
-const layout = __path_views_frontend + 'layouts/layout';
-const pageTitle = 'Đơn hàng của bạn';
-const linkIndex = '/' + collectionName;
+const MainController = require(__path_controllers_frontend + collectionName);
+
 
 /* GET ORDER PAGE */
-router.get('/', async (req, res, next) => {
-	
-	res.render(`${folderView}/index`, { 
-		layout,
-		pageTitle, 
-	});
-});
-
+router.get('/', MainController.index);
 /* GET ORDER DATA */
-router.get('/:code', async (req, res, next) => {
-	const { code } = req.params;
-
-	const data = await OrderModel.getItemFrontend(code);
-	res.send({data});
-});
-
+router.get('/:code', MainController.getOrder);
 /* POST NEW ORDER */
-router.post('/add', async (req, res, next) => {
-	const item = JSON.parse(req.body.data);
-	const user = req.user;
-
-	const data = await OrderModel.saveItem(item, {task: 'add', user});
-	
-	await mailerHelpers.sendMail(user.email, notifyConfigs.MAIL_TITLE_ORDER, util.format(notifyConfigs.MAIL_CONTENT_ORDER, data.code));
-
-	res.send(data);
-});
+router.post('/add', MainController.addOrder);
 
 module.exports = router;
