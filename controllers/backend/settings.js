00145e90916efb41fc8760744e7edbcd174ad024
Quy <hnquy.hbt@gmail.com>
2022-02-15 23:40:49 +0700

    ADD controller settings backend

--- controllers/backend/settings.js
+++ controllers/backend/settings.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-const fs = require('fs');
+const path 				= require('path');
 
-const multer = require('multer');
+const multer 			= require('multer');
 
-const collectionName = 'settings';
-const MainModel = require(__path_models + collectionName);
-const systemConfigs = require(__path_configs + 'system');
-const NotifyConfig = require(__path_configs + 'notify');
-const FileHelpers = require(__path_helpers + 'file');
-const StringHelpers = require(__path_helpers + 'string');
+const collectionName 	= 'settings';
+const MainModel 		= require(__path_models + collectionName);
+const systemConfigs 	= require(__path_configs + 'system');
+const NotifyConfig 		= require(__path_configs + 'notify');
+const FileHelpers 		= require(__path_helpers + 'file');
+const StringHelpers 	= require(__path_helpers + 'string');
 
-const folderView = `${__path_views_admin}pages/${collectionName}`;
-const folderUploads = `${__path_uploads}${collectionName}/`;
-const linkIndex = `/${systemConfigs.prefixAdmin}/${collectionName}`;
-const pageTitle = "Settings";
+const folderView 		= `${__path_views_admin}pages/${collectionName}`;
+const folderUploads 	= `${__path_uploads}${collectionName}/`;
+const linkIndex 		= `/${systemConfigs.prefixAdmin}/${collectionName}`;
+const pageTitle 		= "Settings";
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
@@ -43,47 +40,43 @@ const upload = multer({
 	{ name: 'logo_white', maxCount: 1 },
 ]);
 
-
-// Get FORM --- ADD/EDIT
-router.get('/', async (req, res) => {
-	const messages = req.flash('notify');
-	let emptyItem = {
-		id: '', linkedin: '', twitter: '', google: '', instagram: '',
-		facebook: '', email: '', address: '', copyright: '', logo: '', logo_white: ''
-	};
-	res.locals.sidebarActive = `${collectionName}|form`;
-	let item = await MainModel.getItem();
-	item = item ? item : emptyItem;
-
-	res.render(`${folderView}/form`, { pageTitle, item, messages });
-});
-
-// POST ADD/EDIT
-router.post('/', (req, res) => {
-	upload(req, res, async (err) => {
-		if (err) {
-			req.flash('notify', `danger|Có lỗi khi tải file. Vui lòng kiểm tra kích thước, kiểu file`);
-			return res.redirect(linkIndex);
-		}
-		const item = req.body;
-		const task = item.id ? 'edit' : 'add';
-
-		const fileFields = Object.keys(req.files)
-
-		if (fileFields.length === 0) {
-			
-		} else for(let i = 0; i < fileFields.length; i++) {
-			const files = req.files;
-			const filename = files[fileFields[i]][0].filename;
-			const fieldname = fileFields[i];
-			FileHelpers.removeFile(folderUploads, item[fieldname]);
-			item[fieldname] = filename;
-		}
-
-		await MainModel.saveItem(item, { task });
-		req.flash('notify', `success|Đã lưu cài đặt`);
-		res.redirect(linkIndex);
-	});
-});
-
-module.exports = router;
+module.exports = {
+	getForm: async (req, res) => {
+		const messages = req.flash('notify');
+		let emptyItem = {
+			id: '', linkedin: '', twitter: '', google: '', instagram: '',
+			facebook: '', email: '', address: '', copyright: '', logo: '', logo_white: ''
+		};
+		res.locals.sidebarActive = `${collectionName}|form`;
+		let item = await MainModel.getItem();
+		item = item ? item : emptyItem;
+	
+		res.render(`${folderView}/form`, { pageTitle, item, messages });
+	},
+	postForm: (req, res) => {
+		upload(req, res, async (err) => {
+			if (err) {
+				req.flash('notify', `danger|Có lỗi khi tải file. Vui lòng kiểm tra kích thước, kiểu file`);
+				return res.redirect(linkIndex);
+			}
+			const item = req.body;
+			const task = item.id ? 'edit' : 'add';
+	
+			const fileFields = Object.keys(req.files)
+	
+			if (fileFields.length === 0) {
+				
+			} else for(let i = 0; i < fileFields.length; i++) {
+				const files = req.files;
+				const filename = files[fileFields[i]][0].filename;
+				const fieldname = fileFields[i];
+				FileHelpers.removeFile(folderUploads, item[fieldname]);
+				item[fieldname] = filename;
+			}
+	
+			await MainModel.saveItem(item, { task });
+			req.flash('notify', `success|Đã lưu cài đặt`);
+			res.redirect(linkIndex);
+		});
+	},
+}
