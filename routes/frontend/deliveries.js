a9457fc26ef30ae6c924b4defc8f06f4a8c01320
Quy <hnquy.hbt@gmail.com>
2022-02-16 00:19:31 +0700

    ADD controller deliveries frontend

--- routes/frontend/deliveries.js
+++ routes/frontend/deliveries.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const DeliveryModel = require(__path_models + 'deliveries');
-const DiscountModel = require(__path_models + 'discounts');
+const collectionName = 'deliveries';
+const MainController = require(__path_controllers_frontend + collectionName);
 
-const collectionName = 'discounts';
-const folderView = `${__path_views_frontend}pages/${collectionName}`;
-const layout = __path_views_frontend + 'layouts/layout';
-const pageTitle = 'Discount';
 
-/* GET discount code */
-router.get('/:id', async (req, res, next) => {
-	const id = req.params.id;
-	const data = await DeliveryModel.getItemFrontend(id);
-	
-	res.send({data});
-});
+router.get('/:id', MainController.getDiscountCode);
 
 module.exports = router;
