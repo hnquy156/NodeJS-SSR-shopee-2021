227f44889a1eec221d2e2828785d85f30c046fda
Quy <hnquy.hbt@gmail.com>
2022-02-16 00:06:29 +0700

    ADD controller cart frontend

--- routes/frontend/carts.js
+++ routes/frontend/carts.js
@@ -1,41 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const CartModel = require(__path_models + 'carts');
-const ProductModel = require(__path_models + 'products');
-
 const collectionName = 'carts'
-const folderView = `${__path_views_frontend}pages/${collectionName}`;
-const layout = __path_views_frontend + 'layouts/layout';
-const pageTitle = 'Giỏ hàng';
+const MainController = require(__path_controllers_frontend + collectionName);
 
 
-/* GET API Products in Cart. */
-router.get('/get-products/', async (req, res, next) => {
-	res.send({data: res.locals.cartProducts});
-});
-
-/* GET Add product to cart. */
-router.get('/:task/:cartID/:productID', async (req, res, next) => {
-	const {task, productID, cartID} = req.params;
-	const quantity = +req.query.quantity;
-	
-	const data = await CartModel.saveItem({productID, cartID, quantity}, {task});
-	res.send(data);
-});
-
+router.get('/get-products/', MainController.getProducts);
+router.get('/:task/:cartID/:productID', MainController.addProductsToCart);
 /* GET cart page. */
-router.get('/:id', async (req, res, next) => {
-	const id = req.params.id;
-	// const cartItem = await ProductModel.getItem(id);
-	// const cartProducts = await CartModel.getCartProducts(id, null);
-
-	res.render(`${folderView}/index`, { 
-		pageTitle, 
-		layout, 
-		// cartProducts: cartProducts.products
-	});
-});
+router.get('/:id', MainController.index);
 
 
 module.exports = router;
