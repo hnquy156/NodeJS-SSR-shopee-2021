a500d427a77040e1f225ebb777bfb1126f00b944
Quy <hnquy.hbt@gmail.com>
2022-02-16 00:24:02 +0700

    ADD controller home frontend

--- controllers/frontend/home.js
+++ controllers/frontend/home.js
@@ -1,6 +1,3 @@
-const express = require('express');
-const router = express.Router();
-
 const ProductModel = require(__path_models + 'products');
 const SliderModel = require(__path_models + 'sliders');
 
@@ -9,21 +6,20 @@ const folderView = `${__path_views_frontend}pages/${collectionName}`;
 const layout = __path_views_frontend + 'layouts/layout';
 const pageTitle = 'Trang chủ';
 
-/* GET home page. */
-router.get('/', async (req, res, next) => {
-	const products 		  = await ProductModel.getListFrontend({task: 'products-new'}, null);
-	const specialProducts = await ProductModel.getListFrontend({task: 'products-special'}, null);
-	const soldoutProducts = await ProductModel.getListFrontend({task: 'products-soldout'}, null);
-	const sliders		  = await SliderModel.getListFrontend({task: 'list'}, null);
-	
-	res.render(`${folderView}/index`, { 
-		layout,
-		pageTitle, 
-		products,
-		specialProducts,
-		soldoutProducts,
-		sliders
-	});
-});
-
-module.exports = router;
+module.exports = {
+	index: async (req, res, next) => {
+		const products 		  = await ProductModel.getListFrontend({task: 'products-new'}, null);
+		const specialProducts = await ProductModel.getListFrontend({task: 'products-special'}, null);
+		const soldoutProducts = await ProductModel.getListFrontend({task: 'products-soldout'}, null);
+		const sliders		  = await SliderModel.getListFrontend({task: 'list'}, null);
+		
+		res.render(`${folderView}/index`, { 
+			layout,
+			pageTitle, 
+			products,
+			specialProducts,
+			soldoutProducts,
+			sliders
+		});
+	}
+}
--- routes/frontend/home.js
+++ routes/frontend/home.js
@@ -1,29 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const ProductModel = require(__path_models + 'products');
-const SliderModel = require(__path_models + 'sliders');
-
 const collectionName = 'home';
-const folderView = `${__path_views_frontend}pages/${collectionName}`;
-const layout = __path_views_frontend + 'layouts/layout';
-const pageTitle = 'Trang chủ';
+const MainController = require(__path_controllers_frontend + collectionName);
 
 /* GET home page. */
-router.get('/', async (req, res, next) => {
-	const products 		  = await ProductModel.getListFrontend({task: 'products-new'}, null);
-	const specialProducts = await ProductModel.getListFrontend({task: 'products-special'}, null);
-	const soldoutProducts = await ProductModel.getListFrontend({task: 'products-soldout'}, null);
-	const sliders		  = await SliderModel.getListFrontend({task: 'list'}, null);
-	
-	res.render(`${folderView}/index`, { 
-		layout,
-		pageTitle, 
-		products,
-		specialProducts,
-		soldoutProducts,
-		sliders
-	});
-});
+router.get('/', MainController.index);
 
 module.exports = router;
