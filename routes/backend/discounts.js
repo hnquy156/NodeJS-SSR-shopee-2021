de38ca7dbc7930f7b157c32849f5ca06fe118ec9
Quy <hnquy.hbt@gmail.com>
2022-02-15 22:56:44 +0700

    ADD controller discounts backend

--- routes/backend/discounts.js
+++ routes/backend/discounts.js
@@ -4,176 +4,22 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
 const collectionName = 'discounts';
-const MainModel = require(__path_models + collectionName);
-const UtilsHelpers = require(__path_helpers + 'utils');
-const ParamsHelpers = require(__path_helpers + 'params');
-const NotifyHelpers = require(__path_helpers + 'notify');
-const systemConfigs = require(__path_configs + 'system');
+const MainController = require(__path_controllers_admin + collectionName);
 const Validates = require(__path_validates + collectionName);
 
-const folderView = `${__path_views_admin}pages/${collectionName}`;
-const linkIndex = `/${systemConfigs.prefixAdmin}/${collectionName}`;
-const pageTitle = "Discounts Management";
 
-/* POST change code name discount */
-router.post('/change-name', async (req, res) => {
-	const id		    = ParamsHelpers.getParam(req.body, 'id', '');
-	const name			= ParamsHelpers.getParam(req.body, 'name', '');
-	const task 			= 'change-name';
-	const user 			= req.user;
-
-	const data = await MainModel.changeName(id, name, {task, user});
-	res.send(data);
-});
-
-/* POST change value */
-router.post('/change-value', async (req, res) => {
-	const id		    = ParamsHelpers.getParam(req.body, 'id', '');
-	const value			= ParamsHelpers.getParam(req.body, 'value', '');
-	const task 			= 'change-value';
-	const user 			= req.user;
-
-	const data = await MainModel.changeValue(id, value, {task, user});
-	res.send(data);
-});
-
-/* POST change value */
-router.post('/change-times', async (req, res) => {
-	const id		    = ParamsHelpers.getParam(req.body, 'id', '');
-	const times			= ParamsHelpers.getParam(req.body, 'times', '');
-	const task 			= 'change-times';
-	const user 			= req.user;
-
-	const data = await MainModel.changeTimes(id, times, {task, user});
-	res.send(data);
-});
-
-/* GET SORT */
-router.get('/sort/:sortField/:sortType', async (req, res) => {
-	req.session.sortType		    = ParamsHelpers.getParam(req.params, 'sortType', 'asc');
-	req.session.sortField		    = ParamsHelpers.getParam(req.params, 'sortField', 'ordering');
-
-	res.redirect(linkIndex);
-});
-
-/* POST Delete multi */
-router.post('/change-ordering', async (req, res) => {
-	const id		    = ParamsHelpers.getParam(req.body, 'cid', '');
-	const ordering		= ParamsHelpers.getParam(req.body, 'ordering', '');
-	const task 			= Array.isArray(id) ? 'change-ordering-multi' : 'change-ordering-one';
-
-	const data = await MainModel.changeOrdering(id, ordering, {task});
-	res.send(data);
-	// NotifyHelpers.showNotifyAndRedirect(req, res, linkIndex, {task: 'change-ordering'});
-});
-
-/* GET Delete one */
-router.get('/delete/:id', async (req, res) => {
-	const id		    = ParamsHelpers.getParam(req.params, 'id', '');
-
-	await MainModel.deleteItem(id, {task: 'delete-one'});
-	NotifyHelpers.showNotifyAndRedirect(req, res, linkIndex, {task: 'delete-one'});
-});
-
-/* POST Delete multi */
-router.post('/delete/', async (req, res) => {
-	const id		    = ParamsHelpers.getParam(req.body, 'cid', '');
-
-	const result = await MainModel.deleteItem(id, {task: 'delete-multi'});
-	NotifyHelpers.showNotifyAndRedirect(req, res, linkIndex, {task: 'delete-multi', total: result.deletedCount});
-});
-
-/* GET Change status one */
-router.get('/change-status/:status/:id', async (req, res) => {
-	const currentStatus = ParamsHelpers.getParam(req.params, 'status', 'active');
-	const id		    = ParamsHelpers.getParam(req.params, 'id', '');
-
-	const data = await MainModel.changeStatus(id, currentStatus, {task: 'change-status-one'});
-	res.send(data);
-	// NotifyHelpers.showNotifyAndRedirect(req, res, linkIndex, {task: 'change-status-one'});
-});
-
-/* POST Change status multi */
-router.post('/change-status/:status', async (req, res) => {
-	const currentStatus = ParamsHelpers.getParam(req.params, 'status', 'active');
-	const id		    = ParamsHelpers.getParam(req.body, 'cid', '');
-
-	const result = await MainModel.changeStatus(id, currentStatus, {task: 'change-status-multi'});
-	NotifyHelpers.showNotifyAndRedirect(req, res, linkIndex, {task: 'change-status-multi', total: result.modifiedCount});
-});
-
-/* GET list page. */
-router.get('(/status/:status)?', async (req, res, next) => {
-	const condition = {};
-	const messages	= req.flash('notify');
-	const sortType = ParamsHelpers.getParam(req.session, 'sortType', 'asc');
-	const sortField = ParamsHelpers.getParam(req.session, 'sortField', 'ordering');
-	const sort = {[sortField]: sortType};
-	const currentStatus = ParamsHelpers.getParam(req.params, 'status', 'all');
-	const currentPage = ParamsHelpers.getParam(req.query, 'page', 1);
-	const search_value = ParamsHelpers.getParam(req.query, 'search_value', '');
-	res.locals.sidebarActive = `${collectionName}|list`;
-
-	if (currentStatus !== 'all') condition.status = currentStatus;
-	if (search_value) condition.name = new RegExp(search_value, 'i');
-
-	const pagination = {
-		itemsTotal: await MainModel.countItems(condition),
-		itemsOnPerPage: 6,
-		currentPage,
-		pageRanges : 3,
-	}
-	pagination.pagesTotal = Math.ceil(pagination.itemsTotal / pagination.itemsOnPerPage);
-	const options = {
-		limit: pagination.itemsOnPerPage,
-		skip: (pagination.currentPage - 1) * pagination.itemsOnPerPage,
-		sort,
-	}
-
-	const filterStatus = await UtilsHelpers.createFilterStatus(currentStatus, collectionName, condition);
-	const items = await MainModel.getList(condition, options);
-
-	res.render(`${folderView}/list`, { 
-		pageTitle,
-		messages,
-		items,
-		currentStatus,
-		filterStatus,
-		search_value,
-		pagination,
-		sortType,
-		sortField,
-	});
-});
-
-// Get FORM --- ADD/EDIT
-router.get('/form(/:id)?', async (req, res) => {
-	const id = ParamsHelpers.getParam(req.params, 'id', '');
-	let item = {id: '', name: '', ordering: 1, content: '', times: '', used: 0, value: '', 
-				start: '', end: ''};
-	const errors = [];
-	const pageTitle = id ? 'Edit' : 'Add';
-	item = id ? await MainModel.getItem(id) : item;
-	res.locals.sidebarActive = `${collectionName}|form`;
-	
-	res.render(`${folderView}/form`, {pageTitle, errors, item});
-});
-
-// POST ADD/EDIT
-router.post('/form', Validates.formValidate(body), async (req, res) => {
-	const item = req.body;
-	const errors = validationResult(req).array();
-	const pageTitle = item && item.id ? 'Edit' : 'Add';
-	const task = item && item.id ? 'edit' : 'add';
-	res.locals.sidebarActive = `${collectionName}|form`;
-	
-	if (errors.length > 0) {
-		res.render(`${folderView}/form`, {pageTitle, errors, item});
-
-	} else {
-		await MainModel.saveItem(item, {task});
-		NotifyHelpers.showNotifyAndRedirect(req, res, linkIndex, {task});
-	}
-});
+router.post('/change-name', MainController.changeName);
+router.post('/change-value', MainController.changeValue);
+router.post('/change-times', MainController.changeTimes);
+router.get('/sort/:sortField/:sortType', MainController.sort);
+router.post('/change-ordering', MainController.changeOrdering);
+router.get('/delete/:id', MainController.deleteOne);
+router.post('/delete/', MainController.deleteMulti);
+router.get('/change-status/:status/:id', MainController.changeStatusOne);
+router.post('/change-status/:status', MainController.changeStatusMulti);
+router.get('(/status/:status)?', MainController.index);
+// FORM --- ADD/EDIT
+router.get('/form(/:id)?', MainController.getForm);
+router.post('/form', Validates.formValidate(body), MainController.postForm);
 
 module.exports = router;
