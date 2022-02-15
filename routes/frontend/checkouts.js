605ab169cbdfef28c7559f26230de1a65719ad74
Quy <hnquy.hbt@gmail.com>
2022-02-16 00:12:50 +0700

    ADD controller checkouts frontend

--- routes/frontend/checkouts.js
+++ routes/frontend/checkouts.js
@@ -1,24 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const DeliveryModel = require(__path_models + 'deliveries');
-const DiscountModel = require(__path_models + 'discounts');
-
 const collectionName = 'checkouts';
-const folderView = `${__path_views_frontend}pages/${collectionName}`;
-const layout = __path_views_frontend + 'layouts/layout';
-const pageTitle = 'Thanh toán';
+const MainController = require(__path_controllers_frontend + collectionName);
 
-/* GET home page. */
-router.get('/:id', async (req, res, next) => {
-	const id = req.params.id;
-	const cities = await DeliveryModel.getListFrontend({task: 'list'});
 
-	res.render(`${folderView}/index`, { 
-		pageTitle, 
-		layout, 
-		cities,
-	});
-});
+router.get('/:id', MainController.index);
 
 module.exports = router;
