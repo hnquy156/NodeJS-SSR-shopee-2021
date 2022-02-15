3f60fa87f503fe3bd04b9b450bc8176f9d224fa0
Quy <hnquy.hbt@gmail.com>
2022-02-16 00:32:02 +0700

    ADD controller products frontend

--- controllers/frontend/products.js
+++ controllers/frontend/products.js
@@ -1,6 +1,3 @@
-const express = require('express');
-const router = express.Router();
-
 const ProductModel = require(__path_models + 'products');
 
 const collectionName = 'products'
@@ -8,44 +5,37 @@ const folderView = `${__path_views_frontend}pages/${collectionName}`;
 const layout = __path_views_frontend + 'layouts/layout';
 const pageTitle = 'Sản phẩm';
 
-
-/* get load more */
-router.get('/load-more', async (req, res) => {
-	const page		    = req.query.page;
-	const task 			= 'products-load-more';
-	const user 			= req.user;
+module.exports = {
+	loadmore: async (req, res) => {
+		const page		    = req.query.page;
+		const task 			= 'products-load-more';
+		const user 			= req.user;
+		
+		const data 			= await ProductModel.getListFrontend({task}, {page});
+		
+		res.send({data, user});
+	},
+	changeLike: async (req, res) => {
+		const id		    = req.body.id;
+		const task 			= 'change-like';
+		const user 			= req.user;
+		
+		if (!user) return res.end();
+		
+		const data = await ProductModel.changeLike(id, {task, user});
+		res.send(data);
+	},
+	index: async (req, res, next) => {
+		const id = req.params.id;
+		
+		const productItem  = await ProductModel.getItemFrontend(id);
+		const products 	   = await ProductModel.getListFrontend({task: 'products-in-category'}, {id: productItem.group.id});
 	
-	const data 			= await ProductModel.getListFrontend({task}, {page});
-	
-	res.send({data, user});
-});
-
-/* POST change like  */
-router.post('/change-like/', async (req, res) => {
-	const id		    = req.body.id;
-	const task 			= 'change-like';
-	const user 			= req.user;
-	
-	if (!user) return res.end();
-	
-	const data = await ProductModel.changeLike(id, {task, user});
-	res.send(data);
-	// NotifyHelpers.showNotifyAndRedirect(req, res, linkIndex, {task: 'change-like'});
-});
-
-/* GET product page. */
-router.get('/:id', async (req, res, next) => {
-	const id = req.params.id;
-	
-	const productItem  = await ProductModel.getItemFrontend(id);
-	const products 	   = await ProductModel.getListFrontend({task: 'products-in-category'}, {id: productItem.group.id});
-
-	res.render(`${folderView}/index`, { 
-		pageTitle, 
-		layout, 
-		productItem,
-		products,
-	});
-});
-
-module.exports = router;
+		res.render(`${folderView}/index`, { 
+			pageTitle, 
+			layout, 
+			productItem,
+			products,
+		});
+	},
+}
--- routes/frontend/products.js
+++ routes/frontend/products.js
@@ -1,51 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const ProductModel = require(__path_models + 'products');
-
 const collectionName = 'products'
-const folderView = `${__path_views_frontend}pages/${collectionName}`;
-const layout = __path_views_frontend + 'layouts/layout';
-const pageTitle = 'Sản phẩm';
+const MainController = require(__path_controllers_frontend + collectionName);
 
 
-/* get load more */
-router.get('/load-more', async (req, res) => {
-	const page		    = req.query.page;
-	const task 			= 'products-load-more';
-	const user 			= req.user;
-	
-	const data 			= await ProductModel.getListFrontend({task}, {page});
-	
-	res.send({data, user});
-});
-
-/* POST change like  */
-router.post('/change-like/', async (req, res) => {
-	const id		    = req.body.id;
-	const task 			= 'change-like';
-	const user 			= req.user;
-	
-	if (!user) return res.end();
-	
-	const data = await ProductModel.changeLike(id, {task, user});
-	res.send(data);
-	// NotifyHelpers.showNotifyAndRedirect(req, res, linkIndex, {task: 'change-like'});
-});
-
-/* GET product page. */
-router.get('/:id', async (req, res, next) => {
-	const id = req.params.id;
-	
-	const productItem  = await ProductModel.getItemFrontend(id);
-	const products 	   = await ProductModel.getListFrontend({task: 'products-in-category'}, {id: productItem.group.id});
-
-	res.render(`${folderView}/index`, { 
-		pageTitle, 
-		layout, 
-		productItem,
-		products,
-	});
-});
+router.get('/load-more', MainController.loadmore);
+router.post('/change-like/', MainController.changeLike);
+router.get('/:id', MainController.index);
 
 module.exports = router;
